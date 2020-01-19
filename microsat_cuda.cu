666b9bc56e95cc2862d25718fe9594762670dddc
Giovanni Rosa <g.rosa2@studenti.unimol.it>
2020-01-19 20:04:02 +0100

    fix metriche

--- microsat_cuda.cu
+++ microsat_cuda.cu
@@ -578,7 +578,7 @@ exec_metrics.parse_time = (clock() - start_parse);
 	end = clock();
 	//printf("\n total time: %f s\n", (float)(end - start) / 1000000);
 	exec_metrics.tot_time = (end - start);
-	printf("\n+++ metrics (ms)+++\nfiles count: %d\nparse time: %f\ncuda init time: %f\ncuda solve time: %f\ntot time: %f\n\n", exec_metrics.files_count, exec_metrics.parse_time, exec_metrics.init_time, exec_metrics.solve_time, exec_metrics.tot_time);
+	printf("\n+++ metrics (ms)+++\nfiles count: %d\nparse time: %d\ncuda init time: %d\ncuda solve time: %d\ntot time: %d\n\n", exec_metrics.files_count, exec_metrics.parse_time, exec_metrics.init_time, exec_metrics.solve_time, exec_metrics.tot_time);
 	//printf ("c statistics of %s: mem: %i conflicts: %i max_lemmas: %i\n", argv[1], S.mem_used, S.nConflicts, S.maxLemmas);
 	//printf("\n END \n");
 	return 0;
