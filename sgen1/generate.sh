4fbfe5f869f180279d4f6abde2486581abb04c4b
Giovanni Rosa <g.rosa2@studenti.unimol.it>
2020-01-19 16:29:40 +0100

    aggiunti esperimenti

--- sgen1/generate.sh
+++ sgen1/generate.sh
@@ -1,8 +1,8 @@
 # sat formula generator
-
-mkdir gen20
+n=100
+mkdir gen$n
 
 for i in {1..1000}; do
     echo "generate $i"
-    ./sgen1 -sat -n 20 -s $i > gen20/sat20_$i.cnf
+    ./sgen1 -sat -n 100 -s $i > gen$n/sat$n\_$i.cnf
 done 
