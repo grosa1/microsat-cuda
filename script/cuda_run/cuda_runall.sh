e1aa0fe710c4df33e0debde6a743b8dfe8843dab
Giovanni Rosa <g.rosa1@studenti.unimol.it>
2020-02-06 02:50:11 +0100

    fix log files

--- script/cuda_run/cuda_runall.sh
+++ script/cuda_run/cuda_runall.sh
@@ -1,16 +1,22 @@
 # lancio sequenziale di sat solver
 
 dir=$1
-mcuda=$2
+mcuda_bin=$2
 log_name=$3
 mem=$4
 learn_mem=$5
 learn_clauses=$6
-i=0
+mkdir jobs/$log_name
 
 for f in $dir/*; do
-   ((i++))
-   echo $log_name - $i: $f formulas 
-    timeout 60m sh -c "time ./$mcuda $f $mem $learn_mem $learn_clauses" > cuda_logs/mcuda$log_name-$i.txt
-    echo
+    formula_size=${dir#*/}
+    n_formulas=${f#*/}
+    n_formulas=${n_formulas#*/}
+    out_file=jobs/$log_name/$formula_size\_log$n_formulas\_$log_name.txt
+    
+    echo +++ START $formula_size $n_formulas > $out_file
+    start=$(date +%s%3N)
+    timeout 60m sh -c "./$mcuda_bin $f $mem $learn_mem $learn_clauses" >> $out_file
+    echo TOT_TIME_MILLS=$(( $(date +%s%3N) - $start )) >> $out_file
+    echo +++ END >> $out_file
 done
